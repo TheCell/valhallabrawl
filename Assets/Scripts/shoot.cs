30d4fe10a9803f4f5fe060c0e52f9a3af3854c69
micromic5 <mike.amacker@gmx.ch>
2017-10-04 10:49:57 +0200

    Added Pinsel Prefab and shooting mechanics

---
+++ Assets/Scripts/shoot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoot : MonoBehaviour {
+
+    public GameObject pinselPrefab;
+    public Transform pinselSpawn;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            fire();
+        }
+    }
+    private void fire()
+    {
+        var pinsel = (GameObject)Instantiate(pinselPrefab,
+        pinselSpawn.position,
+        pinselSpawn.rotation);
+        //Add velocity to the pinsel
+        pinsel.GetComponent<Rigidbody>().velocity = pinsel.transform.forward * 60;
+        //Destroy the pinsel
+        Destroy(pinsel, 15.0f);
+    }
+}
