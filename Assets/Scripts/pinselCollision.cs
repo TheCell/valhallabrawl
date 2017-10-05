7b7ddb5b283db6ecb6ae0406874c14c89bf161ff
micromic5 <mike.amacker@gmx.ch>
2017-10-05 09:11:16 +0200

    Shoots destroy hexagons and themself on collision

---
+++ Assets/Scripts/pinselCollision.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pinselCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    void OnCollisionEnter(Collision collision)
+    {
+        // ContactPoint contact = collision.contacts[0];
+        // Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+        // Vector3 pos = contact.point;
+        if (collision.gameObject.tag == "Hexagon")
+        {
+            Destroy(gameObject);
+            Destroy(collision.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
