24dd5217f477abe0a338f364a3f2cba3a5e2f1e2
micromic5 <mike.amacker@gmx.ch>
2017-10-06 13:11:14 +0200

    Added the Deathsphere
    deathsphere can call a event on the player

---
+++ Assets/Scripts/deathTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deathTrigger : MonoBehaviour {
+     public float timerToDestroyPlayer = 2f;
+
+     void OnTriggerExit(Collider collision)
+      {
+         if (collision.gameObject.tag == "Player")
+          {
+            Debug.Log(collision.gameObject.GetComponent<Movement>().player);
+              Destroy(collision.gameObject, timerToDestroyPlayer);
+          }
+      }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    
+}
