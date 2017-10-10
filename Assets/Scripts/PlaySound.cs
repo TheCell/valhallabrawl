343c9438d903d5408b9701e81b5b8c63fc310784
TheCell <simon_hischier@hotmail.com>
2017-10-10 23:17:59 +0200

    added Sound

---
+++ Assets/Scripts/PlaySound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+	public AudioSource startMusicSource;
+	public AudioSource loopMusicSource;
+
+	void Start()
+	{
+		StartCoroutine(playStartSoundAndLoopAfterwards());
+	}
+
+	IEnumerator playStartSoundAndLoopAfterwards()
+	{
+		startMusicSource.Play();
+
+		yield return new WaitForSeconds(loopMusicSource.clip.length);
+		print("end of intro sound");
+		loopMusicSource.Play();
+	}
+}
