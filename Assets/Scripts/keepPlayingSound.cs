1c81df06142ff50b8e1900b2eabaf47c37e25142
TheCell <simon_hischier@hotmail.com>
2017-10-12 17:48:04 +0200

    changed Music
    one continuous track over the game

---
+++ Assets/Scripts/keepPlayingSound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keepPlayingSound : MonoBehaviour
+{
+	private AudioSource audioSources;
+	// Use this for initialization
+	void Start ()
+	{
+		audioSources = gameObject.GetComponent<AudioSource>();
+
+		DontDestroyOnLoad(audioSources);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
