30375bae4ff9ad516fb255a3f44b7e239a99ab31
micromic5 <mike.amacker@gmx.ch>
2017-10-12 15:06:46 +0200

    added crosshair again

---
+++ Assets/Scripts/GUICrosshair.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUICrosshair : MonoBehaviour {
+    public Texture2D crosshairTexture  ;
+    private Rect position;
+    private Rect position2;
+    static bool OriginalOn = true;
+
+    void Start()
+    {
+        position = new Rect((Screen.width/2 + crosshairTexture.width/2) / 2, (Screen.height) / 2, crosshairTexture.width, crosshairTexture.height);
+        position2 = new Rect((Screen.width + Screen.width/2 + crosshairTexture.width/2) / 2, (Screen.height) / 2, crosshairTexture.width, crosshairTexture.height);
+    }
+
+    void OnGUI()
+    {
+        if (OriginalOn == true)
+        {
+            GUI.DrawTexture(position, crosshairTexture);
+            GUI.DrawTexture(position2, crosshairTexture);
+        }
+    }
+}
