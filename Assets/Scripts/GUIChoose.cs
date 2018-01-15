1f80be3a7f9de08c4acb2cdd40ac991a4e115685
micromic5 <mike.amacker@gmx.ch>
2018-01-15 18:40:45 +0100

    controller input for menu

---
+++ Assets/Scripts/GUIChoose.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIChoose : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("CrossUpDown") > 0)
+        {
+            Debug.Log("up");
+        }
+        else if (Input.GetAxis("CrossUpDown") < 0)
+        {
+            Debug.Log("down");
+        }
+        else if(Input.GetAxis("CrossLeftRight") < 0)
+        {
+            Debug.Log("left");
+
+        }
+        else if (Input.GetAxis("CrossLeftRight") > 0)
+        {
+            Debug.Log("right");
+        }
+    }
+}
